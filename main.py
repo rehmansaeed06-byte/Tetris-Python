d3641e675aa41315e9a816c2bf3cb146920c14d5
abdul <rehmansaeed2006@gmail.com>
2026-01-11 22:09:10 +0500

    Implement grid system (rows & columns) #2

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import pygame,sys
+from grid import Grid
 pygame.init()
 dark_blue=(44,44,127)
-screen=pygame.display.set_mode((400,600))
+screen=pygame.display.set_mode((300,600))
 pygame.display.set_caption("Tetris Python")
 clock=pygame.time.Clock()
+game_grid=Grid()
+game_grid.grid[0][0]=1
+game_grid.grid[4][5]=4
+game_grid.grid[6][7]=7
+game_grid.print_grid()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit
             sys.exit()
     screen.fill(dark_blue)
+    game_grid.draw(screen)
     pygame.display.update()
     clock.tick(90)
